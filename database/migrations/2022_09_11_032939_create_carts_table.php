310d7933e99b79ecf9582b85c63d65925244603d
garvyn <garvyntanaka@gmail.com>
2022-10-18 10:51:57 +0700

    Update 2022_09_11_032939_create_carts_table.php

--- database/migrations/2022_09_11_032939_create_carts_table.php
+++ database/migrations/2022_09_11_032939_create_carts_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
      */
     public function up()
     {
+        Schema::dropIfExists('carts');
         Schema::create('carts', function (Blueprint $table) {
             $table->id();
             $table->foreignId('user_id')->constrained()->cascadeOnDelete();
